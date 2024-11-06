276395fae406ea67c35cd11803145c9fccf55b30
senaimussie <128145833+senaimussie@users.noreply.github.com>
2024-11-06 12:53:24 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+=
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+
+async function delayedHelloWorld() {
+  console.log("Hello");
+  await delay(2000); // Wait for 2 seconds
+  console.log("World");
+}
+
+delayedHelloWorld();
